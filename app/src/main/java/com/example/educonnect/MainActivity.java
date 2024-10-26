0cedfbcafb58106e678d5bfd74b247e267b83b31
Muhammad Daniyal Tahir <muhammaddaniyaltahir969@gmail.com>
2024-10-26 15:45:27 +0500

    updates

--- app/src/main/java/com/example/educonnect/MainActivity.java
+++ app/src/main/java/com/example/educonnect/MainActivity.java
@@ -14,21 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView signupLink = findViewById(R.id.signup_link);
-        TextView forgotPasswordLink = findViewById(R.id.forgot_password_link);
-
-        signupLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SignupActivity.class));
-            }
-        });
-
-        forgotPasswordLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ForgotPasswordActivity.class));
-            }
-        });
+//        TextView signupLink = findViewById(R.id.signup);
+//        TextView forgotPasswordLink = findViewById(R.id.forgot_password);
+//
+//        signupLink.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                startActivity(new Intent(MainActivity.this, SignupActivity.class));
+//            }
+//        });
+//
+//        forgotPasswordLink.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                startActivity(new Intent(MainActivity.this, ForgotPasswordActivity.class));
+//            }
+//        });
     }
 }
