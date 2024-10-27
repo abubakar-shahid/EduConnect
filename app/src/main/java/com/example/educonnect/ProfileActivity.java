95f213d4eb04523ea64ca59542f59616b34fb36d
Muhammad Daniyal Tahir <muhammaddaniyaltahir969@gmail.com>
2024-10-27 14:20:34 +0500

    Profiling of tutor has been completed

--- app/src/main/java/com/example/educonnect/ProfileActivity.java
+++ app/src/main/java/com/example/educonnect/ProfileActivity.java
@@ -1,20 +1,21 @@
 package com.example.educonnect;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private TextInputEditText etFullName, etPhoneNumber, etCity;
-    private Spinner spinnerCountryCode, spinnerExperienceLevel, spinnerCountry;
+    private AutoCompleteTextView spinnerCountryCode, spinnerExperienceLevel, spinnerCountry;
     private Button btnEditSave;
     private boolean isEditing = false;
 
@@ -39,7 +40,6 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
-    @SuppressLint("WrongViewCast")
     private void initViews() {
         etFullName = findViewById(R.id.et_full_name);
         etPhoneNumber = findViewById(R.id.et_phone_number);
@@ -47,23 +47,20 @@ public class ProfileActivity extends AppCompatActivity {
         spinnerCountryCode = findViewById(R.id.spinner_country_code);
         spinnerExperienceLevel = findViewById(R.id.spinner_experience_level);
         spinnerCountry = findViewById(R.id.spinner_country);
-//        btnEditSave = findViewById(R.id.btn_edit_save);
+        btnEditSave = findViewById(R.id.btn_edit_save);
     }
 
     private void setupSpinners() {
         ArrayAdapter<CharSequence> countryCodeAdapter = ArrayAdapter.createFromResource(this,
-                R.array.country_codes, android.R.layout.simple_spinner_item);
-        countryCodeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                R.array.country_codes, android.R.layout.simple_dropdown_item_1line);
         spinnerCountryCode.setAdapter(countryCodeAdapter);
 
         ArrayAdapter<CharSequence> experienceLevelAdapter = ArrayAdapter.createFromResource(this,
-                R.array.experience_levels, android.R.layout.simple_spinner_item);
-        experienceLevelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                R.array.experience_levels, android.R.layout.simple_dropdown_item_1line);
         spinnerExperienceLevel.setAdapter(experienceLevelAdapter);
 
         ArrayAdapter<CharSequence> countryAdapter = ArrayAdapter.createFromResource(this,
-                R.array.countries, android.R.layout.simple_spinner_item);
-        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                R.array.countries, android.R.layout.simple_dropdown_item_1line);
         spinnerCountry.setAdapter(countryAdapter);
     }
 
@@ -73,32 +70,52 @@ public class ProfileActivity extends AppCompatActivity {
         etFullName.setText("John Doe");
         etPhoneNumber.setText("1234567890");
         etCity.setText("New York");
-        spinnerCountryCode.setSelection(0);
-        spinnerExperienceLevel.setSelection(1);
-        spinnerCountry.setSelection(0);
+        spinnerCountryCode.setText(spinnerCountryCode.getAdapter().getItem(0).toString(), false);
+        spinnerExperienceLevel.setText(spinnerExperienceLevel.getAdapter().getItem(1).toString(), false);
+        spinnerCountry.setText(spinnerCountry.getAdapter().getItem(0).toString(), false);
     }
 
     private void enableEditing() {
         isEditing = true;
-        etFullName.setEnabled(true);
-        etPhoneNumber.setEnabled(true);
-        etCity.setEnabled(true);
-        spinnerCountryCode.setEnabled(true);
-        spinnerExperienceLevel.setEnabled(true);
-        spinnerCountry.setEnabled(true);
+        setFieldsEnabled(true);
         btnEditSave.setText(R.string.save);
+
+        // Change background tint of editable fields
+        int editableBackgroundColor = getResources().getColor(R.color.editable_background, getTheme());
+        changeEditableBackgroundTint(editableBackgroundColor);
+    }
+
+    private void changeEditableBackgroundTint(int color) {
+        View[] editableViews = {
+            etFullName, etPhoneNumber, etCity,
+            spinnerCountryCode, spinnerExperienceLevel, spinnerCountry
+        };
+
+        for (View view : editableViews) {
+            if (view.getParent() instanceof TextInputLayout) {
+                ((TextInputLayout) view.getParent()).setBoxBackgroundColor(color);
+            }
+        }
     }
 
     private void saveProfile() {
         isEditing = false;
-        etFullName.setEnabled(false);
-        etPhoneNumber.setEnabled(false);
-        etCity.setEnabled(false);
-        spinnerCountryCode.setEnabled(false);
-        spinnerExperienceLevel.setEnabled(false);
-        spinnerCountry.setEnabled(false);
+        setFieldsEnabled(false);
         btnEditSave.setText(R.string.edit);
 
+        // Reset background tint of editable fields
+        int defaultBackgroundColor = getResources().getColor(android.R.color.transparent, getTheme());
+        changeEditableBackgroundTint(defaultBackgroundColor);
+
         // TODO: Save profile data to database or shared preferences
+    }
+
+    private void setFieldsEnabled(boolean enabled) {
+        etFullName.setEnabled(enabled);
+        etPhoneNumber.setEnabled(enabled);
+        etCity.setEnabled(enabled);
+        spinnerCountryCode.setEnabled(enabled);
+        spinnerExperienceLevel.setEnabled(enabled);
+        spinnerCountry.setEnabled(enabled);
     }
 }
